1b27cf359faf4e780f0d425f78f1f268b5f9e4aa
Branden Cash <203336+ammmze@users.noreply.github.com>
2025-12-26 12:26:01 -0700

    debug

--- src/sdk/pwm.cpp
+++ src/sdk/pwm.cpp
@@ -1,6 +1,8 @@
 #include "pwm.hpp"
 #include "hardware/clocks.h"
 #include "hardware/pwm.h"
+#include <cstdio>
+#include <hardware/gpio.h>
 
 namespace nevermore {
 
@@ -19,7 +21,14 @@ uint32_t clock_div(uint32_t source_hz, uint32_t target_hz) {
 uint16_t pwm_gpio_duty(uint8_t gpio, uint16_t duty) {
     uint const slice_num = pwm_gpio_to_slice_num(gpio);
     check_slice_num_param(slice_num);
-    pwm_set_enabled(slice_num, true);
+    if (gpio == 14) {
+        printf("Setting PWM on pin %u with duty %u\n", gpio, duty);
+        auto cfg = pwm_get_default_config();
+        pwm_config_set_freq_hz(cfg, 50);
+        pwm_init(slice_num, &cfg, true);
+        gpio_set_function(gpio, GPIO_FUNC_PWM);
+        pwm_set_enabled(slice_num, true);
+    }
 
     // Use rounding here to set it as accurately as possible
     auto top = pwm_hw->slice[slice_num].top;  // NOLINT
@@ -31,6 +40,7 @@ void pwm_set_gpio_duty(uint8_t gpio, uint16_t duty) {
 }
 
 void pwm_off_gpio(uint8_t gpio) {
+    printf("Turning off PWM on pin %u\n", gpio);
     // Get the slice number and channel for the given GPIO pin
     uint const slice_num = pwm_gpio_to_slice_num(gpio);
 
@@ -44,6 +54,14 @@ void pwm_off_gpio(uint8_t gpio) {
 
     // 3. Revert the pin to standard GPIO function
     // gpio_set_function(gpio, GPIO_FUNC_SIO); // needed?
+    // Change pin function to standard GPIO
+    gpio_init(gpio);
+    gpio_set_function(gpio, GPIO_FUNC_SIO);
+
+    // gpio_set_dir(gpio, GPIO_IN);
+    // Set pin as output and drive it low
+    gpio_set_dir(gpio, GPIO_OUT);
+    gpio_put(gpio, 0);
 }
 
 // stolen/derived from: https://github.com/micropython/micropython/blob/master/ports/rp2/machine_pwm.c
--- src/utility/servo.cpp
+++ src/utility/servo.cpp
@@ -13,6 +13,7 @@ void servo_set(GPIO const pin, ServoRange const& range, BLE::Percentage16_10 con
     if (!pin) return;
 
     if (perc == BLE::NOT_KNOWN) {
+        // pwm_set_gpio_duty(pin, 0);  // 0% is typically considered unset/release
         pwm_off_gpio(pin);  // 0% is typically considered unset/release
         return;
     }
@@ -20,6 +21,7 @@ void servo_set(GPIO const pin, ServoRange const& range, BLE::Percentage16_10 con
     auto value = periodic_waves::remap(
                          perc.value_or(0), 0., 100., range.start.value_or(0), range.end.value_or(0)) /
                  100;
+
     // bump 0 -> 1 so servo doesn't disengage
     auto duty = std::max<uint16_t>(1, uint16_t(UINT16_MAX * value));
     pwm_set_gpio_duty(pin, duty);
